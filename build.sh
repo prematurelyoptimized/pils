2e02ef4c844c130a366d77ccfb95e0b422a31790
bobtpawn <bobtpawn@gmail.com>
2024-02-16 13:14:12 -0700

    Updated build script to use cleaner dir structure and added a gitignore to ignore build artifacts

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-g++ -std=c++17 -g -ftrapv -o solvetest test/solvetest.cpp
-g++ -std=c++17 -g -ftrapv -o gmptest test/gmptest.cpp -lgmpxx -lgmp
+mkdir -p bin/test
+g++ -std=c++17 -g -ftrapv -o bin/test/solvetest test/solvetest.cpp
+g++ -std=c++17 -g -ftrapv -o bin/test/gmptest test/gmptest.cpp -lgmpxx -lgmp
