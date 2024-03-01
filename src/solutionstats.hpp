06812b9b438b7493f2ce55920f1ecde5f9fc53ed
bobtpawn <bobtpawn@gmail.com>
2024-02-29 17:57:27 -0700

    fix #2: Added implementation of all-but-one gcds.  Still needs tests

--- src/solutionstats.hpp
+++ src/solutionstats.hpp
@@ -5,7 +5,13 @@ enum SolutionStatus { CONVERGED, INFEASIBLE };
 
 struct SolutionStats {
 	size_t pivots;
+	size_t divisibility_bound_improvements;
 	SolutionStatus status;
+
+	SolutionStats(void) {
+		pivots = 0;
+		divisibility_bound_improvements = 0;
+	}
 };
 
 #endif
